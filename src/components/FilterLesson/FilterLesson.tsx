76bc6c404a1de7aea046e843f79a87ea8e230ff5
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2023-07-30 06:49:29 +0600

    Creating FilterLesson Component

---
+++ src/components/FilterLesson/FilterLesson.tsx
@@ -0,0 +1,54 @@
+import { DatePicker, Select, Space } from "antd"
+import { LessonStatus, ReportStatus } from "../../utils/types";
+import "./FilterLesson.scss"
+import { CustomTag } from "../Tags/CustomTag";
+
+
+const { RangePicker } = DatePicker;
+
+
+const optionsLesson: { value: LessonStatus }[] = [
+  { value: "на отмене" },
+  { value: "отменено" },
+  { value: "назначается" },
+  { value: "новое" },
+  { value: "новое время" },
+  { value: "перенесено" }
+];
+
+const optionsReport: { value: ReportStatus }[] = [
+  { value: "есть ошибки" },
+  { value: "на проверке" },
+  { value: "нет отчета" },
+  { value: "подготовлен" },
+  { value: "принят" }
+];
+
+//Development only
+const optionsGroup: { value: string }[] = [
+  { value: "255-06" },
+  { value: "255-04" },
+  { value: "255-10" },
+  { value: "255-11" },
+  { value: "255-123" }
+];
+
+
+
+export const FilterLesson: React.FC = () => {
+
+  return (
+    <Space wrap className="filter-lesson">
+      <Select style={{ minWidth: 120 }} mode="multiple" showArrow allowClear placeholder="Статус урока"
+        options={optionsLesson} tagRender={({ value }) => (<CustomTag status={value} />)} />
+
+      <Select style={{ minWidth: 130 }} mode="multiple" showArrow allowClear placeholder="Статус отчета"
+        options={optionsReport} tagRender={({ value }) => (<CustomTag status={value} />)} />
+
+      <Select style={{ minWidth: 100 }} mode="multiple" showArrow allowClear placeholder="Группа"
+        options={optionsGroup} />
+
+      <RangePicker showTime={true} style={{ minWidth: 170 }} inputReadOnly format="DD.MM.YY" />
+    </Space>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.scss"
+import ruRU from 'antd/lib/locale/ru_RU';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+import { ConfigProvider } from 'antd';
+
+dayjs.locale('ru');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={ruRU}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
